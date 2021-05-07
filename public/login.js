bb0a0981586689aa0136109f57410cc89ba1e859
Giuliano Damico <65154075+GDeDev@users.noreply.github.com>
2021-05-07 19:20:26 -0300

    Version 2
    * Carga general
    
    * Retoques para validar

---
+++ public/login.js
@@ -0,0 +1,23 @@
+alert("Revisa tu bandeja de entrada para validar el mail")
+const loginForm = document.getElementById('login-form');
+loginForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const loginEmail = loginForm['login-email'].value;
+    const loginPassword = loginForm['login-password'].value;
+    // console.log(loginEmail, loginPassword);
+    auth.signInWithEmailAndPassword(loginEmail, loginPassword).then(() => {
+        var user = firebase.auth().currentUser;
+        
+        if (user.emailVerified) {
+           
+            console.log('login success');
+            location = "index.html";
+            
+        } else {
+            alert("Mail no valido")
+        }
+    }).catch(err => {
+        // console.log(err.message);
+    })
+})
+
